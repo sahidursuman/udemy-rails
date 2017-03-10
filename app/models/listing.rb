40c69d11479fdbf93b1645ca1bb16a3421393529
Tatsuya Yagi <user.email>
2017-03-10 12:39:43 +0900

    part4-1 done

---
+++ app/models/listing.rb
@@ -0,0 +1,10 @@
+class Listing < ApplicationRecord
+  belongs_to :user
+
+  # means required
+  validates :home_type, presence: true
+  validates :pet_type, presence: true
+  validates :pet_size, presence: true
+  validates :breeding_years, presence: true
+
+end
