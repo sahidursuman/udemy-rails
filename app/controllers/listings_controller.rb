7d8a49f4bb1ee31af382cc1ce826e4c0e9376f6c
Tatsuya Yagi <user.email>
2017-03-15 10:18:11 +0900

    part4-3-1 done

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -14,6 +14,15 @@ class ListingsController < ApplicationController
   end
 
   def create
+    # パラメータとともに現在のユーザーのリスティングを作成
+    @listing = current_user.listings.build(listing_params)
+
+    if @listing.save
+      redirect_to edit_listing_path(@listing), notice: "リスティングを作成・保存しました"
+    else
+      redirect_to new_listing_path, notice: "リスティングを作成・保存できませんでした"
+    end
+
   end
 
   def edit
@@ -21,4 +30,10 @@ class ListingsController < ApplicationController
 
   def update
   end
+
+  private
+  def listing_params
+    params.require(:listing).permit(:home_type, :pet_type, :breeding_years, :pet_size)
+  end
+
 end
