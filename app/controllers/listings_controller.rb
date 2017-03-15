b8426b54e69f6d62f4028868b0e310b175799a47
Tatsuya Yagi <user.email>
2017-03-15 09:47:07 +0900

    part4-1 done

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,22 @@
+class ListingsController < ApplicationController
+  before_action :authenticate_user!
+
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  # get 'listings/index'
+  #
+  # get 'listings/show'
+  #
+  # get 'listings/new'
+  #
+  # get 'listings/create'
+  #
+  # get 'listings/edit'
+  #
+  # get 'listings/update'
+
+  resources :listings
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => 'registrations' }
   root :to => 'pages#index'
 
