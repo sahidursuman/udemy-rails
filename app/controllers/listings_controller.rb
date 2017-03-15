ea06c6d4d9c36415f9b6c1a172f65ed46cfb4c49
Tatsuya Yagi <user.email>
2017-03-16 08:47:27 +0900

    part8-2 done

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_listing, only: [:update, :basics, :description, :address, :price, :photos, :calendar, :bankaccount, :publish]
+  before_action :set_listing, only: [:show, :update, :basics, :description, :address, :price, :photos, :calendar, :bankaccount, :publish]
 
 
   def index
@@ -8,6 +8,7 @@ class ListingsController < ApplicationController
   end
 
   def show
+    @photos = @listing.photos
   end
 
   def new
