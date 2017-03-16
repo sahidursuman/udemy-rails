6e5bddf2edc38a6567eaaf91fa118d350ed91587
Tatsuya Yagi <user.email>
2017-03-16 13:03:20 +0900

    part11-4 done

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,6 +273,6 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
 
-  config.omniauth :facebook, ENV["facebook_app_id"], ENV["facebook_app_secret"], scope: 'email', info_fields: 'email,name'
+  config.omniauth :facebook, ENV["facebook_app_id"], ENV["facebook_app_secret"], scope: 'email', info_fields: 'email,name', secure_image_url: true
 
 end
