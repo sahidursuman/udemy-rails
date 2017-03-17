a5ae39ed7faf9f708fc4b97b55eb1732909d102e
Tatsuya Yagi <user.email>
2017-03-17 11:32:21 +0900

    part12-12 done. application_fee cannot be transfered if you are logged in via Facebook and make a reservation of because of Stripe security policy. if the destination, which is stripe_user_id, is same as posseser of api key in this application, transaction is denied.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -62,19 +62,23 @@ class ReservationsController < ApplicationController
       # Charge
       amount = params[:reservation][:total_price]
 
+      fee = (amount.to_i * 0.1).to_i
+
       # Calculate the fee amount that goes to the application.
       begin
         charge_attrs = {
           amount: amount,
           currency: user.currency || 'jpy',
           source: params[:token],
-          description: "Test Charge via Stripe Connect"
+          description: "Test Charge via Stripe Connect",
+          application_fee: fee
         }
 
       # Use the platform's access token, and specify the
       # connected account's user id as the destination so that
       # the charge is transferred to their account.
       charge_attrs[:destination] = user.stripe_user_id
+
       charge = Stripe::Charge.create( charge_attrs )
 
         #have to edit view template to show html in flash
