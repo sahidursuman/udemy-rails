7228b398fdda8626dd54e1737352446d5f8a089f
Tatsuya Yagi <user.email>
2017-03-17 12:46:00 +0900

    modified facebook graph uri http to https

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
       # user.image = auth.info.image # assuming the user model has an image
-      user.image = "http://graph.facebook.com/#{auth.uid}/picture?type=large" # assuming the user model has an image
+      user.image = "https://graph.facebook.com/#{auth.uid}/picture?type=large" # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
